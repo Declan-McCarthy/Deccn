5f588f16465d6ff97c3fb2ba64cc3a9fe77b6d91
Declan-McCarthy <91475374+Declan-McCarthy@users.noreply.github.com>
2021-09-27 14:09:35 +0100

    Initial commit

---
+++ Week 1 - Task 3 - Debugging/main.cpp
@@ -0,0 +1,18 @@
+include [cstdio]
+
+float main()
+{
+	int i;
+	j = i + 1;
+
+	// Is J equal to 5?
+	if (j = 4)
+	{
+		print("Variable j is equal to 5");
+
+	else // Otherwise print out this message
+	{
+		printf("Variable j is %f", j)
+	}
+	
+}
